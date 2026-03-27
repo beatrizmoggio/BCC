5a535e4c952dd65c13977249d8024184ffbe92ee
beatriz moggio <103965037+beatrizmoggio@users.noreply.github.com>
2026-03-26 22:41:23 -0300

    Add files via upload

---
+++ 4° Período/Análise e Estrutura de Dados 2/Ordenação/anagramas.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <algorithm>
+#include "utils.h"
+
+using namespace std;
+
+#define DEBUG 1
+
+/*
+retorna true se s e t são anagramas. Caso contrário, retorna false.
+s e t são strings terminadas em 0.
+*/
+
+bool sao_anagramas(string& s, string& t){
+    // tamanhos diferentes
+    if (s.size() != t.size()) return false;
+    
+    // ordena
+    sort(s.begin(), s.end());
+    sort(t.begin(), t.end());
+
+    // retorna true se forem iguais, false se diferentes
+    return s == t;
+}
+
+int main(int argc, char** argv){
+
+    string s1(argv[1]);
+    string s2(argv[2]);
+
+    #if DEBUG
+    cout << sao_anagramas(s1, s2) << "\n";
+    #endif
+    
+    return EXIT_SUCCESS;
+}
